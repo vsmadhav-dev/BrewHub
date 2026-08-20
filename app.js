3705926b8e0032281bf950a7ea244ea2b123e06c
MADHAV ANAND <m47903237@gmail.com>
2026-08-20 13:47:41 +0530

    Add Code to services file

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-window.addEventListener("DOMContentLoaded" , () => {
+import Store from './services/Store.js';
+import API from './services/API.js';
+import {loadData} from "./services/menu.js";
 
+window.app = {}
+app.store = Store;
+window.addEventListener("DOMContentLoaded" , async() => {
+loadData();
 })
