f06853618858cbf293c517ca755ee80db076faee
MADHAV ANAND <m47903237@gmail.com>
2026-08-21 14:14:22 +0530

    Fix bugs

--- services/Router.js
+++ services/Router.js
@@ -1,22 +1,21 @@
 const Router = {
     init: () => {
-  document.querySelectorAll('a.navlink').forEach(a => {
-      a.addEventListener('click' , event =>{
-          event.preventDefault();
-          const url1 = event.target.href;
-          const url2 = event.target.getAttribute(
-              'href');
-          Router.go(url1);
+        document.querySelectorAll('a.navlink').forEach(a => {
+            a.addEventListener('click', event => {
+                event.preventDefault();
+                const url1 = event.target.getAttribute('href');
+                Router.go(url1);
+            });
+        });
 
-          Router.go(location.pathname);
-      })
-  })
-    } ,
-    go: (route , addToHistory == true) = {
- console.log(`Going to ${route}`);
- if (addToHistory) {
-     history.pushState()
- }
+        Router.go(location.pathname, false);
+    },
+    go: (route, addToHistory = true) => { // Fixed '==' to '='
+        console.log(`Going to ${route}`);
+        if (addToHistory) {
+            history.pushState({ route }, '', route);
+        }
     }
-}
+};
+
 export default Router;
