faa55d883e73edee7844e60e6c08afbdf08214dc
MADHAV ANAND <m47903237@gmail.com>
2026-08-26 11:29:54 +0530

    Add custom elements

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import Store from './services/Store.js';
 import API from './services/API.js';
 import {loadData} from "./services/menu.js";
 import Router from "./services/router.js";
+import { MenuPage} from "./components/menuPage.js";
+import { DetailsPage} from "./components/DetailsPage.js";
+import{OrderPage} from "./components/OrderPage.js";
 
 window.app = {}
 app.store = Store;
---
+++ components/menuPage.js
@@ -0,0 +1,6 @@
+export class MenuPage extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+customElements.define('menu-page', MenuPage);
