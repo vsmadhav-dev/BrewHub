680c0285b1c58e1b9700dd04efca39bb9ec258fb
MADHAV ANAND <m47903237@gmail.com>
2026-08-26 12:02:45 +0530

    Add shadow DOM and add styles

--- components/menuPage.js
+++ components/menuPage.js
@@ -1,12 +1,21 @@
 export class MenuPage extends HTMLElement {
     constructor() {
         super();
+ this.root = this.attachShadow({ mode: 'open' });
+ const styles = document.createElement('style');
+this.root.appendChild(styles);
+ async function loadCss() {
 
+    const request = await  fetch('/components/MenuPage.css');
+    const css = await request.text();
+    styles.textContent = css;
+ }
+ loadCss();
     }
     connectedCallback() {
         const template = document.getElementById('menu-page-template')
         const content = template.content.cloneNode(true);
-        this.appendChild(content);
+        this.root.appendChild(content);
     }
 }
 customElements.define('menu-page', MenuPage);
