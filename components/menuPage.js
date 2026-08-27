a4fd08f7126397dc6177d64391b4b50d92c32209
MADHAV ANAND <m47903237@gmail.com>
2026-08-27 14:30:07 +0530

    Add proxy and load menu

--- components/menuPage.js
+++ components/menuPage.js
@@ -1,21 +1,49 @@
 export class MenuPage extends HTMLElement {
     constructor() {
         super();
- this.root = this.attachShadow({ mode: 'open' });
- const styles = document.createElement('style');
-this.root.appendChild(styles);
- async function loadCss() {
+        this.root = this.attachShadow({ mode: 'open' });
+        const styles = document.createElement('style');
+        this.root.appendChild(styles);
 
-    const request = await  fetch('/components/MenuPage.css');
-    const css = await request.text();
-    styles.textContent = css;
- }
- loadCss();
+        async function loadCss() {
+            const request = await fetch('/components/MenuPage.css');
+            const css = await request.text();
+            styles.textContent = css;
+        }
+        loadCss();
     }
+
     connectedCallback() {
-        const template = document.getElementById('menu-page-template')
+        const template = document.getElementById('menu-page-template');
         const content = template.content.cloneNode(true);
         this.root.appendChild(content);
+
+        window.addEventListener('menuchange', (event) => {
+            this.render();
+        });
+
+        this.render();
+    }
+
+    render() {
+        const menuContainer = this.root.querySelector('#menu');
+        if (!menuContainer) return;
+
+        if (app.store.menu) {
+            menuContainer.innerHTML = '';
+
+            for (let i = 0; i < app.store.menu.length; i++) {
+                const liCategory = document.createElement('li');
+                liCategory.innerHTML = `
+                    <h3>${app.store.menu[i].name}</h3>
+                    <ul class="category"></ul>
+                `;
+                menuContainer.appendChild(liCategory);
+            }
+        } else {
+            menuContainer.innerHTML = 'loading..';
+        }
     }
 }
+
 customElements.define('menu-page', MenuPage);
--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,16 @@ const store =
         menu: null ,
         cart: {}
     }
-    export default store;
+    const proxiedStore  = new Proxy(store, {
+        set(target, key, value) {
+            target[key] = value;
+            if(key === 'menu'){
+                window.dispatchEvent(new Event('menuchange'));
+            }
+            if(key === 'cart'){
+                window.dispatchEvent(new Event('cartchange'));
+            }
+            return true;
+        }
+    })
+    export default proxiedStore;
