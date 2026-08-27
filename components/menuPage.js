ecee746815d61e39b1fcd27ffb52408aab8c25f4
MADHAV ANAND <m47903237@gmail.com>
2026-08-27 14:52:07 +0530

    Fix error

--- components/menuPage.js
+++ components/menuPage.js
@@ -2,13 +2,16 @@ export class MenuPage extends HTMLElement {
     constructor() {
         super();
         this.root = this.attachShadow({ mode: 'open' });
-        const styles = document.createElement('style');
-        this.root.appendChild(styles);
 
-        async function loadCss() {
-            const request = await fetch('/components/MenuPage.css');
-            const css = await request.text();
-            styles.textContent = css;
+        this.styles = document.createElement('style');
+        this.root.appendChild(this.styles);
+
+         async function loadCss() {
+
+                const request = await  fetch('/components/MenuPage.css');
+                const css = await request.text();
+               this.styles.textContent = css;
+
         }
         loadCss();
     }
@@ -39,6 +42,12 @@ export class MenuPage extends HTMLElement {
                     <ul class="category"></ul>
                 `;
                 menuContainer.appendChild(liCategory);
+
+                app.store.menu.forEach((item) => {
+                    const product = document.createElement('product-item');
+                    product.dataset.item = JSON.stringify(item)
+                   liCategory.querySelector('ul')
+                })
             }
         } else {
             menuContainer.innerHTML = 'loading..';
